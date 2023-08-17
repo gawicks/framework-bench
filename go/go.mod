49d48adb395eea2570a0449f754fedaa31307735
harithawickremasinghe <gawicks@gmail.com>
2023-08-17 17:45:48 +1000

    introduce fasthttp

--- go/go.mod
+++ go/go.mod
@@ -5,6 +5,8 @@ go 1.21.0
 require github.com/jackc/pgx/v5 v5.4.3
 
 require (
+	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/fasthttp/router v1.4.20 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.8.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -12,6 +14,12 @@ require (
 	github.com/jackc/pgtype v1.6.2 // indirect
 	github.com/jackc/pgx/v4 v4.10.1 // indirect
 	github.com/jackc/puddle v1.1.3 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
 
@@ -21,6 +29,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/savsgio/atreugo/v11 v11.10.1
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
--- go/main.go
+++ go/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	pgx "github.com/jackc/pgx/v5/pgxpool"
+	"github.com/valyala/fasthttp"
 )
 
 type User struct {
@@ -25,46 +24,38 @@ type Server struct {
 	db *pgx.Pool
 }
 
-func (server *Server) helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world")
-}
-func (server *Server) userHandler(w http.ResponseWriter, r *http.Request) {
-	results, err := server.db.Query(context.Background(), "SELECT * FROM USERS WHERE dob > DATE('1990-01-01')")
+func (server *Server) userHandler(ctx *fasthttp.RequestCtx) {
+	results, err := server.db.Query(context.Background(), "SELECT * FROM USERS WHERE dob > '1990-01-01'")
 	if err != nil {
 		log.Fatal(err)
 	}
-	var users []*User
+	defer results.Close()
+
+	var users []User
 	for results.Next() {
 		var u User
 		err = results.Scan(&u.Id, &u.Username, &u.Name, &u.Gender, &u.Dob, &u.Email, &u.Phone)
 		if err != nil {
 			log.Fatal(err)
 		}
-		users = append(users, &u)
+		users = append(users, u)
 	}
 	jsonData, err := json.Marshal(users)
 	if err != nil {
 		log.Fatal(err)
 	}
-	w.Header().Set("Connection", "keep-alive")
-	w.Header().Set("Transfer-Encoding", "chunked")
-	w.Header().Set("Keep-Alive", "timeout=5")
-	w.Write(jsonData)
+	ctx.SetBody(jsonData)
 }
 
 func main() {
-	conn := "postgresql://admin:admin@127.0.0.1:5432/db?sslmode=disable"
+	conn := "user=admin password=admin host=localhost port=5432 dbname=db sslmode=disable pool_max_conns=40"
 	db, err := pgx.New(context.Background(), conn)
+	defer db.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 	server := Server{db}
-	http.HandleFunc("/", server.userHandler)
-	fmt.Println("Server is running on port 8003...")
-	http.ListenAndServe(":8003", nil)
-}
-
-func (server *Server) getUsers() (interface{}, error) {
-	rows, err := server.db.Query(context.Background(), "SELECT * FROM USERS WHERE dob > DATE('1990-01-01')")
-	return &rows, err
+	if err := fasthttp.ListenAndServe(":8003", server.userHandler); err != nil {
+		log.Fatalf("Error in ListenAndServe: %v", err)
+	}
 }
