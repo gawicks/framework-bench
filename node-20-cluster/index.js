423512c0b7d25d74c46284c131c4f51773a17ace
harithawickremasinghe <gawicks@gmail.com>
2023-08-18 17:46:23 +1000

    add prexit to node-sp

--- node-20-cluster/index.js
+++ node-20-cluster/index.js
@@ -1,12 +1,15 @@
 import cluster from "cluster"
 import * as http from "http"
 import postgres from "postgres";
+import prexit from 'prexit'
+
 const workers = 4;
 
 const sql = postgres({ 
     username: 'admin',
     password: 'admin',
     database: 'db',
+    max_lifetime: 1
 });
 function getUsers() {
     const users = sql`SELECT * FROM USERS WHERE dob > '1990-01-01'`;
@@ -17,10 +20,16 @@ if (cluster.isPrimary) {
     cluster.fork();
   }
 } else {
-  http.createServer(async (req, res) => {
+  const server = http.createServer(async (req, res) => {
     const users = await getUsers();
     res.end(JSON.stringify(users));
   }).listen(8001);
+  prexit(async () => {
+    await sql.end({ timeout: 5 })
+    await server.close()
+    console.log("EXIT CALLED")
+  })
 
-  console.log(`Worker ${cluster.worker.id} started`);
+  console.log(`Worker ${cluster.worker.id} started`);  
 }
+
--- node-20/index.js
+++ node-20/index.js
@@ -1,7 +1,8 @@
 import * as http from "http";
 import postgres from "postgres";
+import prexit from 'prexit';
 
-const sql = postgres({ 
+const sql = postgres({
     username: 'admin',
     password: 'admin',
     database: 'db',
@@ -10,8 +11,14 @@ function getUsers() {
     const users = sql`SELECT * FROM USERS WHERE dob > '1990-01-01'`;
     return users;
 }
-const server = http.createServer(async(req, res) => {
+const server = http.createServer(async (req, res) => {
     const users = await getUsers();
     res.end(JSON.stringify(users));
 })
 server.listen(8000);
+console.log("Server started");
+prexit(async () => {
+    await sql.end({ timeout: 5 });
+    await server.close();
+    console.log("EXIT CALLED");
+})
